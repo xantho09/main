c53d700a29001f015856bf55155396070bc9f6d8
FongYuan <42345573+FongYuan@users.noreply.github.com>
2018-10-15 11:34:47 +0800

    Remove Name validity predicate

--- src/main/java/seedu/address/model/loan/Name.java
+++ src/main/java/seedu/address/model/loan/Name.java
@@ -12,27 +12,24 @@ public class Name extends DataField<String> {
     public static final String MESSAGE_NAME_CONSTRAINTS =
         "Names should only contain alphanumeric characters and spaces, and it should not be blank";
 
-    /*
-     * The first character of the address must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
-     */
-    public static final Predicate<String> VALIDITY_PREDICATE =
-        test -> test.matches("[\\p{Alnum}][\\p{Alnum} ]*");
-
     /**
      * Constructs a {@code Name}.
      *
      * @param name A valid name.
      */
     public Name(String name) {
-        super(MESSAGE_NAME_CONSTRAINTS, VALIDITY_PREDICATE, Function.identity(), name);
+        super(MESSAGE_NAME_CONSTRAINTS, Name::isValidName, Function.identity(), name);
     }
 
     /**
      * Returns true if a given string is a valid name.
      */
     public static boolean isValidName(String objString) {
-        return VALIDITY_PREDICATE.test(objString);
+        /*
+         * The first character of the address must not be a whitespace,
+         * otherwise " " (a blank string) becomes a valid input.
+         */
+        return objString.matches("[\\p{Alnum}][\\p{Alnum} ]*");
     }
 
 }
