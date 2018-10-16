5fe07cba5af13f3774776ce952a9f25d4d7a6409
FongYuan <42345573+FongYuan@users.noreply.github.com>
2018-10-17 07:26:57 +0800

    Add Bike Exceptions

---
+++ src/main/java/seedu/address/model/bike/exceptions/DuplicateBikeException.java
@@ -0,0 +1,11 @@
+package seedu.address.model.bike.exceptions;
+
+/**
+ * Signals that the operation will result in duplicate Bikes (Bikes are considered duplicates if they have the same
+ * identity).
+ */
+public class DuplicateBikeException extends RuntimeException {
+    public DuplicateBikeException() {
+        super("Operation would result in duplicate bikes");
+    }
+}
