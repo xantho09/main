f30228f46f8e5258b36f3f018e49cfe9f052c2b7
Ang Wei Neng <weineng.a@gmail.com>
2018-11-06 22:43:16 +0800

    Fix issue from dry run (#255)
    * Fix issue #208
    
    * Add future release table
    
    * Fix NRIC error message, issue #201
    
    * Fix user guide
    
    * Fix user guide
    
    * Fix lint
    
    * Update docs
    
    * Update NRIC error message
    
    * Fix lint

--- src/main/java/loanbook/commons/core/Messages.java
+++ src/main/java/loanbook/commons/core/Messages.java
@@ -13,7 +13,8 @@ public class Messages {
     public static final String MESSAGE_INVALID_OLD_PASS = "The old password entered is incorrect!";
     public static final String MESSAGE_SAME_AS_CURRENT_PASSWORD = "The password provided is same as before!";
     public static final String MESSAGE_INVALID_DATE_FORMAT =
-            "Dates provided are in an invalid format. Please use the format YYYY-MM-DD.";
+            "Dates provided are either an invalid date or they are in an invalid format.\n"
+            + "Please use the format YYYY-MM-DD.";
     public static final String MESSAGE_INVALID_DATE_RANGE =
             "Dates provided are not in a valid range. The end date cannot be before the start date.";
     public static final String MESSAGE_AUTHEN_FAILURE = "Your password might be wrong "
